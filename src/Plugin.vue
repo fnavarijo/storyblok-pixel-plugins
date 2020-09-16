9ac5c0373ab47a93506c9ff8c0f76a178528545b
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2020-09-16 12:39:05 -0600

    Adding Pixel List Plugin

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -1,20 +1,29 @@
 <template>
-  <div>
-    Google snippet preview:
-    <div class="p-metatags__preview">
-      <div class="p-metatags__google-title">{{ model.title || 'Your title' }}</div>
-      <div class="p-metatags__google-link">yoursite.com/example</div>
-      <div class="p-metatags__google-description">{{ model.description || 'Your description' }}</div>
-    </div>
-    <div class="uk-form-row">
-      <label>Meta Title</label>
-      <input type="text" placeholder="Your title" v-model="model.title" class="uk-width-1-1">
-    </div>
-
-    <div class="uk-form-row">
-      <label>Meta description</label>
-      <textarea rows="4" placeholder="Your description" v-model="model.description" class="uk-width-1-1"></textarea>
-    </div>
+  <div class="pixel-list">
+    <ol class="pixel-list__list uk-margin-bottom-remove">
+      <li
+        v-for="(item, index) in model.items"
+        :key="index"
+        class="pixel-list__list-item uk-flex uk-flex-middle"
+      >
+        <input
+          v-model="model.items[index]"
+          class="uk-form-small uk-width-1-1"
+          :aria-label="`List item ${index}`"
+        >
+        <a
+          class="assets__item-trash"
+          aria-label="Remove item"
+          @click="removeItem(index)"
+        >
+          <i class="uk-icon-minus-circle"></i>
+        </a>
+      </li>
+    </ol>
+    <a class="blok__full-btn uk-margin-small-top" @click="addItem">
+      <i class="uk-icon-plus-circle uk-margin-small-right" />
+      Add Item
+    </a>
   </div>
 </template>
 
@@ -24,42 +33,42 @@ export default {
   methods: {
     initWith() {
       return {
-        // needs to be equal to your storyblok plugin name
-        plugin: 'my-plugin-name',
-        title: '',
-        description: ''
+        plugin: 'pixel-list',
+        items: [''],
       }
     },
     pluginCreated() {
       // eslint-disable-next-line
       console.log('View source and customize: https://github.com/storyblok/storyblok-fieldtype')
+    },
+    addItem() {
+      this.model.items.push('');
+    },
+    removeItem(index) {
+      this.model.items = this.model.items.filter((_, i) => i !== index);
     }
   },
   watch: {
-    'model': {
-      handler: function (value) {
+    model: {
+      deep: true,
+      handler (value) {
         this.$emit('changed-model', value);
       },
-      deep: true
     }
   }
 }
 </script>
 
 <style>
-  .p-metatags__google-title {
-    color: blue;
-    text-decoration: underline;
+  .pixel-list {
+    padding-left: 0;
   }
 
-  .p-metatags__google-link {
-    color: green;
+  .pixel-list__list-item {
+    list-style-type: none;
   }
 
-  .p-metatags__preview {
-    margin: 5px 0 15px;
-    padding: 10px;
-    color: #000;
-    background: #FFF;
+  .pixel-list__list-item + .pixel-list__list-item {
+    margin-top: 5px;
   }
 </style>
